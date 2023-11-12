26798c9aaf9960962076d4fff3de73cc53618f23
Kura2004 <megurinua@gmail.com>
2023-11-13 01:13:28 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -12,16 +12,19 @@ int sum(int num, int* array)
 	int sum = 0;
 
 	for (int i = 0; i < num; i++)
-	{
 		sum += array[i];
-	}
-
+	
 	return sum;
 }
 
 // num個の整数の平均を得る
 int average(int num, int* array)
 {
-	return 0x7fffffff;// 未実装
+	int ave = 0;
+	int Sum = sum(num, array);
+
+	ave = Sum / num;
+
+	return ave;// 未実装
 }
 
